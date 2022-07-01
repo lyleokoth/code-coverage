a4a4003a088d272a91ee56c03e6008c7887630f4
lyleokoth <okothlyleochieng@gmail.com>
2022-07-01 18:45:34 +0300

    fix: removes the extra argument from the create badge function.

--- api/blueprints/api/views.py
+++ api/blueprints/api/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """This module contains the routes associated with the auth Blueprint."""
 import json
+import random
 
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
@@ -109,58 +110,73 @@ def get_badge():  # pylint: disable=R0911
             "style": "style-name"
             }
     """
-    # user = User.query.filter_by(username=username).first()
-    # if not user:
-    #     data = {
-    #         "schemaVersion": 1,
-    #         "label": "error",
-    #         "message": "invalid username",
-    #         "color": "red",
-    #         "labelColor": "green",
-    #         "style": "for-the-badge"
-    #     }
-    #     return jsonify(data), 400
+    username = 'lyleokoth'
+    projectname = 'lyleokoth/flask-social-auth'
+    badgename = 'coverage-total'
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        data = {
+            "schemaVersion": 1,
+            "label": "error",
+            "message": "invalid username",
+            "color": "red",
+            "labelColor": "green",
+            "style": "for-the-badge"
+        }
+        return data, 200
 
-    # projectname = f'{username}/{projectname}'
-    # project = Project.query.filter_by(name=projectname).first()
-    # if not project:
-    #     data = {
-    #         "schemaVersion": 1,
-    #         "label": "error",
-    #         "message": "invalid projectname",
-    #         "color": "red",
-    #         "labelColor": "green",
-    #         "style": "for-the-badge"
-    #     }
-    #     return jsonify(data), 400
+    project = Project.query.filter_by(name=projectname).first()
+    if not project:
+        data = {
+            "schemaVersion": 1,
+            "label": "error",
+            "message": "invalid projectname",
+            "color": "red",
+            "labelColor": "green",
+            "style": "for-the-badge"
+        }
+        return data, 200
 
-    # run = Run.query.filter_by(project_id=project.id).first()
-    # if not run:
-    #     data = {
-    #         "schemaVersion": 1,
-    #         "label": "error",
-    #         "message": "missing test data",
-    #         "color": "red",
-    #         "labelColor": "green",
-    #         "style": "for-the-badge"
-    #     }
-    #     return jsonify(data), 400
+    run = Run.query.filter_by(project_id=project.id).first()
+    if not run:
+        data = {
+            "schemaVersion": 1,
+            "label": "error",
+            "message": "missing test data",
+            "color": "red",
+            "labelColor": "green",
+            "style": "for-the-badge"
+        }
+        return data, 200
 
-    # if badgename not in ['coverage-total']:
-    #     data = {
-    #         "schemaVersion": 1,
-    #         "label": "error",
-    #         "message": "Invalid badge name",
-    #         "color": "red",
-    #         "labelColor": "green",
-    #         "style": "for-the-badge"
-    #     }
-    #     return jsonify(data), 400
+    if badgename not in ['coverage-total']:
+        data = {
+            "schemaVersion": 1,
+            "label": "error",
+            "message": "Invalid badge name",
+            "color": "red",
+            "labelColor": "green",
+            "style": "for-the-badge"
+        }
+        return data, 200
 
-    run_id = Run.query.filter_by(project_id=1).order_by(desc(Run.id)).first().id
+    colors = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'grey']
+    styles = ['flat', 'plastic', 'flat-square', 'for-the-badge', 'social']
+    data = {
+        "schemaVersion": 1,
+        "label": "name",
+        "message": "lyle okoth",
+        "color": random.choice(colors),
+        "labelColor": random.choice(colors),
+        "style": random.choice(styles)
+    }
 
-    badge = Badge.query.filter_by(run_id=run_id).first()
-    # print(badge)
+    return data, 200
 
-    badge_schema = BadgeSchema()
-    return badge_schema.dump(badge), 200
+    # run_id = Run.query.filter_by(project_id=1).order_by(desc(Run.id)).first().id
+
+    # badge = Badge.query.filter_by(run_id=run_id).first()
+    # # print(badge)
+
+    # badge_schema = BadgeSchema()
+    # return badge_schema.dump(badge), 200
